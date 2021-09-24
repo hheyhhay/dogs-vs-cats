3eb88ddc2b90ae9e949f7c7359cecef3c8f58d10
Hayley Dixon <hdixon37@gmail.com>
2021-09-24 16:19:24 -0600

    Add styling to Favorites component

--- src/Favorites.js
+++ src/Favorites.js
@@ -1,19 +1,22 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-// import './Favorites.css'
+import './Favorites.css'
 
 const Favorites = ({favoriteCats, favoriteDogs, clearFavorites }) => {
   const totalFavorite = favoriteCats.concat(favoriteDogs)
   const favoriteCards = totalFavorite.map(image => {
     return(
-      <img src={image.url} alt={`Super cute picture of a ${image.type}`}className={`${image.type}-image`} />
+      <div className='image'>
+        <img src={image.url} alt={`Super cute picture of a ${image.type}`}className={`${image.type}-image`} />
+      </div>
     )
   })
   return (
     <div className='favorite-section'>
+    <h1>You picked some really cute pictures</h1>
       <div className='favorite-container'>{favoriteCards}</div>
       <Link to={'/'}>
-        <button onClick={clearFavorites}>Take Test </button>
+        <button className='result-button' onClick={clearFavorites}>Take Test </button>
       </Link>
     </div>
   )
