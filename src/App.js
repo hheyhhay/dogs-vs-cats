19ff519dc220c6e041e5db225dad2fec0d17b9f5
Hayley Dixon <hdixon37@gmail.com>
2021-09-24 10:53:40 -0600

    Move fetch calls and state from Game to App, add route for Results

--- src/App.js
+++ src/App.js
@@ -1,16 +1,59 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
 import Game from './Game';
 import Results from './Results';
 import { Route, Switch, Link } from 'react-router-dom';
+import { fetchCatData, fetchDogData } from './apiCalls';
+import { organizeCat, organizeDog } from './util';
 
 import './App.css';
 
 const App = () => {
   const [animalChoice, setAnimalChoice] = useState('');
+  const [favoriteCats, setFavoriteCats] = useState([]);
+  const [favoriteDogs, setFavoriteDogs] = useState([]);
+  const [catPicture, setCatPicture] =useState({});
+  const [dogPicture, setDogPicture] = useState({});
+   // move to App -> make result a sibling
+
+  const [imageCount,  setImageCount] = useState(0);
+  const [error, setError] = useState('')
+
+  const getData = async () => {
+    try {
+      const catData = await fetchCatData()
+      const simpleCatData = organizeCat(catData)
+
+      setCatPicture(simpleCatData)
+
+      const dogData = await fetchDogData()
+      const simpleDogData = organizeDog(dogData)
+
+      setDogPicture(simpleDogData)
+    }
+    catch(error)
+    {
+      setError(error.message)
+    }
+  }
+
+  useEffect(() => {
+    getData();
+    }, [])
+
+  const handleChange = (e, image) => {
+
+    if (e.target.className === 'cat-image'){
+        setFavoriteCats([...favoriteCats, image])
+    } else {
+      setFavoriteDogs([...favoriteDogs, image])
+    }
+    setImageCount(imageCount+1)
+    getData()
+  }
 
   const onChange = (e) => {
-    e.preventDefault()
+    // e.preventDefault()
     setAnimalChoice(e.target.value)
   }
   const onClick = (e) => {
@@ -20,27 +63,35 @@ const App = () => {
 
   return (
     <main className='App' onChange = {(e) => onChange(e)}>
-      <h1 className='header'> Do you like Dogs or Cats? </h1>
-      <Switch>
+      <h1 className='header'> Are you a dog person or a cat person? </h1>
+
       <Route exact path='/'>
         <div className='radio-choice'>
           <input type='radio' value='dog' name='animal-choice'/> Dogs!
           <input type='radio' value='cat' name='animal-choice'/> Cats!
-        </div>
+
         <Link to={'/game'} >
         {animalChoice &&  <button>Start Game</button> }
         </Link>
         {!animalChoice && <div>Choose an Animal, please</div>}
+        </div>
       </Route>
       <Route exact path='/game'>
         <Game
             animalChoice={animalChoice}
+            handleChange={handleChange}
+            catPicture={catPicture}
+            dogPicture={dogPicture}
+            imageCount={imageCount}
             />
       </Route>
-      <Route exact path='/results'>
-        <Results />
+      <Route exact path='/results' >
+        <Results
+          animalChoice={animalChoice}
+          favoriteCats={favoriteCats}
+          favoriteDogs={favoriteDogs}
+          />
       </Route>
-      </Switch>
 
 
     </main>
