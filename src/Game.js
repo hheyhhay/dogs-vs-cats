b595ece2e91b12d7da83f712329a99f369b8b323
Hayley Dixon <hdixon37@gmail.com>
2021-09-24 16:19:43 -0600

    Update styling

--- src/Game.js
+++ src/Game.js
@@ -11,7 +11,7 @@ const Game = ( {animalChoice, handleChange, catPicture, dogPicture, imageCount }
   return (
     <div className='game-container'>
       <h1>Who is cuter?</h1>
-    {imageCount < 3 && <div className='quiz-container'>
+    {imageCount < 7 && <div className='quiz-container'>
       <div className='poloriod-card'>
         {catPicture && <img src={catPicture.url} alt={`Super cute picture of a ${catPicture.type}`} className='cat-image' onClick={(e) => handleChange(e, catPicture)} />}
       </div>
@@ -22,7 +22,7 @@ const Game = ( {animalChoice, handleChange, catPicture, dogPicture, imageCount }
         {dogPicture && <img src={dogPicture.url} alt={`Super cute picture of a ${dogPicture.type}`} className='dog-image' onClick={(e) => handleChange(e, dogPicture)} />}
       </div>
     </div>}
-    { imageCount === 3 && <Redirect to='/results'
+    { imageCount === 7 && <Redirect to='/results'
                             />
     }
 
