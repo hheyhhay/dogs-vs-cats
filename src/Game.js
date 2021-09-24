6366995a441ee7b9667dbbb1727ccadda655cb46
Hayley Dixon <hdixon37@gmail.com>
2021-09-24 10:55:05 -0600

    Add redirect for REsults page, take out passing down information

--- src/Game.js
+++ src/Game.js
@@ -1,63 +1,39 @@
 import React, { useState, useEffect } from 'react';
 import { fetchCatData, fetchDogData } from './apiCalls';
 import { organizeCat, organizeDog } from './util';
-import { Route, Link } from 'react-router-dom';
+import { Route, Link, Redirect } from 'react-router-dom';
 import Results from './Results'
 
 import './Game.css'
 
-const Game = ( {animalChoice} ) => {
-  const [catPicture, setCatPicture] =useState({});
-  const [dogPicture, setDogPicture] = useState({});
-  const [favoriteCats, setFavoriteCats] = useState([])
-  const [favoriteDogs, setFavoriteDogs] = useState([]);
-  const [imageCount,  setImageCount] = useState(0);
-  const [error, setError] = useState('')
+const Game = ( {animalChoice, handleChange, catPicture, dogPicture, imageCount } ) => {
 
-  const getData = async () => {
-    try {
-      const catData = await fetchCatData()
-      const simpleCatData = organizeCat(catData)
 
-      setCatPicture(simpleCatData)
-
-      const dogData = await fetchDogData()
-      const simpleDogData = organizeDog(dogData)
-
-      setDogPicture(simpleDogData)
-    }
-    catch(error)
-    {
-      setError(error.message)
-    }
-  }
-
-  useEffect(() => {
-    getData();
-    }, [])
-
-  const handleChange = (e) => {
-
-    if (e.target.className === 'cat-image'){
-        setFavoriteCats([...favoriteCats, catPicture])
-    } else {
-      setFavoriteDogs([...favoriteDogs, dogPicture])
-    }
-    setImageCount(imageCount+1)
-    getData()
-  }
+  // const handleChange = (e) => {
+  //
+  //   if (e.target.className === 'cat-image'){
+  //       setFavoriteCats([...favoriteCats, catPicture])
+  //   } else {
+  //     setFavoriteDogs([...favoriteDogs, dogPicture])
+  //   }
+  //   setImageCount(imageCount+1)
+  //   getData()
+  // }
 
   return (
     <div>
-
-    {catPicture && imageCount < 3 && <img src={catPicture.url} alt={`Super cute picture of a ${catPicture.type}`} className='cat-image' onClick={(e) => handleChange(e)} />}
-    {imageCount < 3 &&  <p>cats</p> && <p>or</p> }
-    {dogPicture && imageCount < 3 && <img src={dogPicture.url} alt={`Super cute picture of a ${dogPicture.type}`} className='dog-image' onClick={(e) => handleChange(e)} />}
-    {imageCount < 3 &&   <p>dogs</p> } 
-    { imageCount === 3 && <Results
-                            favoriteDogs={favoriteDogs}
-                            favoriteCats={favoriteCats}
-                            animalChoice={ animalChoice }
+    {imageCount < 3 && <div className='quiz-container'>
+      <div className='poloriod-card'>
+        {catPicture && <img src={catPicture.url} alt={`Super cute picture of a ${catPicture.type}`} className='cat-image' onClick={(e) => handleChange(e, catPicture)} />}
+      </div>
+        <div className='or-div'>
+          <p>or</p>
+        </div>
+      <div className='poloriod-card'>
+        {dogPicture && <img src={dogPicture.url} alt={`Super cute picture of a ${dogPicture.type}`} className='dog-image' onClick={(e) => handleChange(e, dogPicture)} />}
+      </div>
+    </div>}
+    { imageCount === 3 && <Redirect to='/results'
                             />
     }
 
@@ -67,7 +43,7 @@ const Game = ( {animalChoice} ) => {
 }
 
 export default Game;
-
+// redirect at the last one to /results
 // <Link to={'/results'}>
 //  <button>see your results</button>
 // </Link>
