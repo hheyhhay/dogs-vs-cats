c69ffc676c5c8c7e16ac29c3d624dfbed5cd8116
Hayley Dixon <hdixon37@gmail.com>
2021-09-27 13:59:59 -0600

    Change response code from 400 to 404

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -16,7 +16,7 @@ export const fetchDogData = async () => {
 
   let response = await fetch('https://dog.ceo/api/breeds/image/random')
 
-  if (response.status === 400) {
+  if (response.status === 404) {
     throw new Error('404 error - Destination not found')
   } else if (response.status === 500) {
     throw new Error('500 error - Can\'t access pictures right now')
