67b5ebcf1ec9a213824b87626dcf5931a1c24a9c
Hayley Dixon <hdixon37@gmail.com>
2021-10-30 15:50:48 -0600

    Combine intercepts into single command

--- cypress/integration/app_specs.js
+++ cypress/integration/app_specs.js
@@ -1,19 +1,26 @@
  describe('Application flow on main page', () => {
    beforeEach(() => {
      cy.visit("http://whodoyoulove.surge.sh")
-     cy.interceptAPI("cat", 'https://api.thecatapi.com/v1/images/search')
-     cy.interceptAPI("dog", 'https://dog.ceo/api/breeds/image/random')
+     
+     cy.interceptAPI("dog", 'https://dog.ceo/api/breeds/image/random', "cat", 'https://api.thecatapi.com/v1/images/search')
+
      cy.visit("http://whodoyoulove.surge.sh")
    })
 
    it('Should go to main page on page load', () => {
-     cy.url().should('eq', 'http://whodoyoulove.surge.sh/')
+     cy.url()
+      .should('eq', 'http://whodoyoulove.surge.sh/')
    })
+
    it('Should be able to choice a animal preference and start the game on a new page', () => {
+    cy.get('[type=radio]')
+      .first()
+      .check()
 
-    cy.get('[type=radio]').first().check()
-    cy.get('button').click()
+    cy.get('button')
+      .click()
 
-    cy.url().should('eq', 'http://whodoyoulove.surge.sh/game')
+    cy.url()
+      .should('eq', 'http://whodoyoulove.surge.sh/game')
    })
  })
