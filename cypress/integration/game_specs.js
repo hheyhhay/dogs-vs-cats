ca395676e513e9e7f425490b2ca384df1653fb2d
Hayley Dixon <hdixon37@gmail.com>
2021-10-30 16:02:20 -0600

    Combine intercepts into single command

--- cypress/integration/game_specs.js
+++ cypress/integration/game_specs.js
@@ -1,67 +1,25 @@
 describe('Application flow for game', () => {
 
   it('Should be able to start the game and be routed to new page', () => {
-    cy.interceptAPI("cat", 'https://api.thecatapi.com/v1/images/search')
-    cy.interceptAPI("dog", 'https://dog.ceo/api/breeds/image/random')
+    cy.interceptAPI("dog", 'https://dog.ceo/api/breeds/image/random', "cat", 'https://api.thecatapi.com/v1/images/search')
+
     cy.visit("http://whodoyoulove.surge.sh/")
 
-    cy.get('[type=radio]').first().check()
-    cy.get('button').click()
-    cy.url().should('include', "game")
+    cy.get('[type=radio]')
+      .first()
+      .check()
 
+    cy.get('button')
+      .click()
+
+    cy.url()
+      .should('eq', 'http://whodoyoulove.surge.sh/game')
   })
+
   it('Should prompt the player to choose between two images of which is cuter', () => {
-
-    cy.get('img').should(($image) => {
-      expect($image).to.have.length(2)
-
-      const classes = $image.map((i, el) => {
-        return Cypress.$(el).attr('class')
-      })
-
-      expect(classes.get()).to.deep.eq([
-        'cat-image',
-        'dog-image'
-      ])
-    })
-  })
-
-  it('Should have 7 rounds of images and will show results', () => {
-
-      cy.interceptAPI("cat", 'https://api.thecatapi.com/v1/images/search')
-      cy.interceptAPI("dog", 'https://dog.ceo/api/breeds/image/random')
-      cy.get('.cat-image').click()
-
-      cy.interceptAPI("cat", 'https://api.thecatapi.com/v1/images/search')
-      cy.interceptAPI("dog", 'https://dog.ceo/api/breeds/image/random')
-      cy.get('.cat-image').click()
-
-      cy.interceptAPI("cat", 'https://api.thecatapi.com/v1/images/search')
-      cy.interceptAPI("dog", 'https://dog.ceo/api/breeds/image/random')
-      cy.get('.cat-image').click()
-
-      cy.interceptAPI("cat", 'https://api.thecatapi.com/v1/images/search')
-      cy.interceptAPI("dog", 'https://dog.ceo/api/breeds/image/random')
-      cy.get('.cat-image').click()
-
-      cy.interceptAPI("cat", 'https://api.thecatapi.com/v1/images/search')
-      cy.interceptAPI("dog", 'https://dog.ceo/api/breeds/image/random')
-      cy.get('.dog-image').click()
-
-      cy.interceptAPI("cat", 'https://api.thecatapi.com/v1/images/search')
-      cy.interceptAPI("dog", 'https://dog.ceo/api/breeds/image/random')
-      cy.get('.dog-image').click()
-
-      cy.interceptAPI("cat", 'https://api.thecatapi.com/v1/images/search')
-      cy.interceptAPI("dog", 'https://dog.ceo/api/breeds/image/random')
-      cy.get('.dog-image').click()
-
-      cy.url().should('include', "results")
-
-      cy.get('.result-statement').should('have.text', 'You are not a dog person, but a cat person')
-
-      cy.get('img').should(($image) => {
-        expect($image).to.have.length(4)
+    cy.get('img')
+      .should(($image) => {
+        expect($image).to.have.length(2)
 
         const classes = $image.map((i, el) => {
           return Cypress.$(el).attr('class')
@@ -69,26 +27,81 @@ describe('Application flow for game', () => {
 
         expect(classes.get()).to.deep.eq([
           'cat-image',
-          'cat-image',
-          'cat-image',
-          'cat-image'
+          'dog-image'
         ])
       })
+  })
+
+  it('Should have 7 rounds of images and will show results', () => {
+    cy.interceptAPI("dog", 'https://dog.ceo/api/breeds/image/random', "cat", 'https://api.thecatapi.com/v1/images/search')
+
+    cy.get('.cat-image')
+      .click()
+
+    cy.interceptAPI("dog", 'https://dog.ceo/api/breeds/image/random', "cat", 'https://api.thecatapi.com/v1/images/search')
+
+    cy.get('.cat-image')
+      .click()
+
+    cy.interceptAPI("dog", 'https://dog.ceo/api/breeds/image/random', "cat", 'https://api.thecatapi.com/v1/images/search')
+
+    cy.get('.cat-image')
+      .click()
+
+    cy.interceptAPI("dog", 'https://dog.ceo/api/breeds/image/random', "cat", 'https://api.thecatapi.com/v1/images/search')
+
+    cy.get('.cat-image')
+      .click()
+
+    cy.interceptAPI("dog", 'https://dog.ceo/api/breeds/image/random', "cat", 'https://api.thecatapi.com/v1/images/search')
+
+    cy.get('.dog-image')
+      .click()
+
+    cy.interceptAPI("dog", 'https://dog.ceo/api/breeds/image/random', "cat", 'https://api.thecatapi.com/v1/images/search')
+
+    cy.get('.dog-image')
+      .click()
+
+    cy.interceptAPI("dog", 'https://dog.ceo/api/breeds/image/random', "cat", 'https://api.thecatapi.com/v1/images/search')
+
+    cy.get('.dog-image')
+      .click()
+
+    cy.url()
+      .should('eq', 'http://whodoyoulove.surge.sh/results')
+
+    cy.get('.result-statement')
+      .should('have.text', 'You are not a dog person, but a cat person')
+
+    cy.get('img').should(($image) => {
+      expect($image).to.have.length(4)
+        const classes = $image.map((i, el) => {
+          return Cypress.$(el).attr('class')
+        })
+
+      expect(classes.get()).to.deep.eq([
+        'cat-image',
+        'cat-image',
+        'cat-image',
+        'cat-image'
+      ])
     })
+  })
 
-    it('Should be able to click the favorites button', () => {
-      cy.get('.favorite-button')
-        .click()
+  it('Should be able to click the favorites button', () => {
+    cy.get('.favorite-button')
+      .click()
 
-      cy.url().should('eq', 'http://whodoyoulove.surge.sh/favorites')
-    })
+    cy.url()
+      .should('eq', 'http://whodoyoulove.surge.sh/favorites')
+  })
 
-    it('Should be able to go restart quiz from results page', () => {
-      cy.get('[href="/"] > .result-button')
-        .click()
+  it('Should be able to go restart quiz from results page', () => {
+    cy.get('[href="/"] > .result-button')
+      .click()
 
-      cy.url().should('eq', 'http://whodoyoulove.surge.sh/')
-
-    })
-
-})
+    cy.url()
+      .should('eq', 'http://whodoyoulove.surge.sh/')
+  })
+});
