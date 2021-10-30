9c8c3ed28631e0cc5723cd21e6c511b495dcfd06
Hayley Dixon <hdixon37@gmail.com>
2021-10-30 15:35:51 -0600

    Update test from localhost to deployed url

--- cypress/integration/app_specs.js
+++ cypress/integration/app_specs.js
@@ -1,27 +1,19 @@
  describe('Application flow on main page', () => {
    beforeEach(() => {
-     cy.visit("localhost:3000")
+     cy.visit("http://whodoyoulove.surge.sh")
      cy.interceptAPI("cat", 'https://api.thecatapi.com/v1/images/search')
      cy.interceptAPI("dog", 'https://dog.ceo/api/breeds/image/random')
-     cy.visit("localhost:3000")
+     cy.visit("http://whodoyoulove.surge.sh")
    })
 
+   it('Should go to main page on page load', () => {
+     cy.url().should('eq', 'http://whodoyoulove.surge.sh/')
+   })
    it('Should be able to choice a animal preference and start the game on a new page', () => {
 
     cy.get('[type=radio]').first().check()
     cy.get('button').click()
-    cy.url().should('include', "game")
-   })
-   // it('Should load an error if cannot access server', () => {
-   //   cy.visit('http://localhost:3000/')
-   //   cy.intercept('GET',
-   //     'https://api.thecatapi.com/v1/images/search',
-   //     {
-   //       statusCode: 500,
-   //       body: {
-   //         message: '500 error - Can\'t access pictures right now'}
-   //     })
-   //      cy.visit('http://localhost:3000')
-   // })
 
+    cy.url().should('eq', 'http://whodoyoulove.surge.sh/game')
+   })
  })
