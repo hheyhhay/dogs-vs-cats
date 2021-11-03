3c760c97c042142b2c85665a374946b4b25d5377
Hayley Dixon <hdixon37@gmail.com>
2021-11-02 22:14:34 -0600

    Change fetch to be dynamic

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,29 +1,25 @@
-export const fetchCatData = async () => {
+const catURL = 'https://api.thecatapi.com/v1/images/search';
+const dogURL = 'https://dog.ceo/api/breeds/image/random'
 
-  let response = await fetch('https://api.thecatapi.com/v1/images/search')
 
-  if (response.status === 404) {
-    throw new Error('404 error - Destination not found')
-  } else if (response.status === 500) {
-    throw new Error('500 error - Can\'t access pictures right now')
+export const fetchData = (species) => {
+
+  let targetURL = "";
+
+  if (species === "cat" ) {
+    targetURL = catURL
   }
-  
-  let cats = await response.json();
-
-  return cats
-};
-
-export const fetchDogData = async () => {
-
-  let response = await fetch('https://dog.ceo/api/breeds/image/random')
-
-  if (response.status === 404) {
-    throw new Error('404 error - Destination not found')
-  } else if (response.status === 500) {
-    throw new Error('500 error - Can\'t access pictures right now')
+  if (species === "dog" ) {
+    targetURL = dogURL
   }
 
-  let dogs = await response.json();
-
-  return dogs
+ return fetch(targetURL)
+    .then(res => {
+      if (res.status === 404) {
+        throw new Error('404 error - Destination not found')
+      } else if (res.status === 500) {
+        throw new Error('500 error - Can\'t access pictures right now')
+      }
+      return res.json()
+    })
 };
